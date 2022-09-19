a421758335f0436ab74099979955feae2061144f
Vegard Løkken <vegard@loekken.org>
2022-09-19 22:16:12 +0200

    Check result of ioctl for errors
    When getting term size, the call to ioctl could fail.

--- cnake.c
+++ cnake.c
@@ -4,14 +4,19 @@
 #include <signal.h> // signal
 #include <sys/ioctl.h> // ioctl
 #include <unistd.h> // STDOUT_FILENO
+#include <stdio.h> // fprintf
 
 int main()
 {
-    term_setup();
-
     // Get terminal window size
     struct winsize size;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &size) == -1)
+    {
+        fprintf(stderr, "error: failed to get term size\n");
+        return -1;
+    }
+
+    term_setup();
 
     t_game *game = new_game(size.ws_row, size.ws_col);
 
