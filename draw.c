f712f675acdcfbba6a4e470566d0cc1251a53507
Vegard Løkken <vegard@loekken.org>
2022-09-19 22:44:28 +0200

    Make it pretty(?) with colors
    Replacing lines and characters with solid color blocks in an attempt to
    improve the visuals. Next step would be using unicode characters, but
    that should be possible to toggle with a cli flag.

--- draw.c
+++ draw.c
@@ -10,50 +10,37 @@ void clear_screen()
 
 void draw_frame(int width, int height)
 {
+    printf("\033[100m");
+
     int i;
 
     for (i = 1; i <= width; i++)
     {
         // Top row
-        printf("\033[0;%dH-", i);
+        printf("\033[0;%dH ", i);
         // Bottom row
-        printf("\033[%d;%dH-", height, i);
+        printf("\033[%d;%dH ", height, i);
     }
 
     for (i = 1; i <= height; i++)
     {
         // Left column
-        printf("\033[%d;0H|", i);
+        printf("\033[%d;0H ", i);
         // Right column
-        printf("\033[%d;%dH|", i, width);
+        printf("\033[%d;%dH ", i, width);
     }
+
+    printf("\033[0m");
 }
 
 void draw_snake(const t_snake *snake)
 {
     if (snake->next)
     {
-        printf("\033[%d;%dH+", snake->next->pos->row, snake->next->pos->col);
+        printf("\033[%d;%dH\033[42m \033[0m", snake->next->pos->row, snake->next->pos->col);
     }
 
-    // Move and print head
-    char head;
-    switch (snake->dir)
-    {
-        case LEFT:
-            head = '<';
-            break;
-        case RIGHT:
-            head = '>';
-            break;
-        case UP:
-            head = 'A';
-            break;
-        case DOWN:
-            head = 'V';
-            break;
-    }
-    printf("\033[%d;%dH%c", snake->pos->row, snake->pos->col, head);
+    printf("\033[%d;%dH\033[102;30m \033[0m", snake->pos->row, snake->pos->col);
 }
 
 void clear_snake(const t_snake *snake)
@@ -70,7 +57,9 @@ void draw_candy(t_pos **candy)
     for (int i = 0; i < N_CANDY; i++)
     {
         if (candy[i]->row > 0)
-            printf("\033[%d;%dHX", candy[i]->row, candy[i]->col);
+        {
+            printf("\033[%d;%dH\033[41m \033[0m", candy[i]->row, candy[i]->col);
+        }
     }
 }
 
@@ -81,20 +70,21 @@ void clear_candy(const t_pos *candy)
 
 void draw_count_down(const t_pos *pos, int n)
 {
-    printf("\033[%d;%dH%d", pos->row, pos->col, n);
+    int color = n == 3 ? 91 : n == 2 ? 93 : 92;
+    printf("\033[%d;%dH\033[%d;1m%d\033[0m", pos->row, pos->col, color, n);
 }
 
 void draw_game_over(const t_pos *pos)
 {
-    printf("\033[%d;%dH%s", pos->row, pos->col - 4, "GAME OVER");
+    printf("\033[%d;%dH\033[31;1m%s\033[0m", pos->row, pos->col - 4, "GAME OVER");
 }
 
 void draw_statusbar(int row, char speed, int points, int highscore)
 {
     printf("\033[%d;1H", row);
-    printf(" SPEED: %d", speed);
-    printf(" POINTS: %d", points);
-    printf(" HIGHSCORE: %d", highscore);
+    printf(" \033[4mSPEED\033[0m: %d", speed);
+    printf(" \033[4mPOINTS\033[0m: %d", points);
+    printf(" \033[4mHIGHSCORE\033[0m: %d", highscore);
 }
 
 void clear_statusbar(int row)
