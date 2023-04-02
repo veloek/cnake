a92b4965ca875a365dfe390f65a8ab6de6824d90
Vegard Løkken <vegard@loekken.org>
2023-04-02 13:49:11 +0200

    Add description to util tests

--- util_test.c
+++ util_test.c
@@ -15,35 +15,54 @@ exit
 #define INT_MAX  2147483647
 #define INT_MIN -2147483648
 
-void test(int n, int size, char* expected)
+static int n_runs;
+static int n_success;
+
+static void test(char* desc, int n, int size, char* expected)
 {
     char buf[size];
     int written = util_itoa(n, buf, size);
     int cmp = strcmp(buf, expected);
     if (cmp == 0)
     {
-        printf("SUCC: test(%d, %d): '%s' (%d)\n", n, size, buf, written);
+        printf("SUCCESS: %s(%d, %d): '%s'(%d)\n",
+                desc, n, size, buf, written);
+        n_success++;
     }
     else
     {
-        printf("FAIL: test(%d, %d): '%s' (%d), expected: %s\n",
-                n, size, buf, written, expected);
+        printf("FAILURE: %s(%d, %d): '%s'(%d), expected: %s\n",
+                desc, n, size, buf, written, expected);
     }
+    n_runs++;
 }
 
 int main()
 {
-    test(1, 0, "");
-    test(1, 1, "");
-    test(1, 2, "1");
-    test(1, 3, "1");
-    test(-1, 2, "-");
-    test(-1, 3, "-1");
-    test(123, 3, "12");
-    test(123, 10, "123");
-    test(-123, 3, "-1");
-    test(-123, 10, "-123");
-    test(INT_MAX, 16, "2147483647");
-    test(INT_MIN+1, 16, "-2147483647");
-    test(INT_MIN, 16, "-"); // Breaks due to overflow
+    test("iota_should_return_empty_string_given_buffer_size_is_1",
+            1, 1, "");
+    test("iota_should_return_full_1_given_buffer_size_is_2",
+            1, 2, "1");
+    test("iota_shoud_return_no_more_than_1_given_buffer_size_is_3",
+            1, 3, "1");
+    test("iota_shoud_return_only_sign_given_buffer_size_is_2",
+            -1, 2, "-");
+    test("iota_should_return_full_-1_given_buffer_size_is_3",
+            -1, 3, "-1");
+    test("iota_should_return_12_out_of_123_given_buffer_size_is_3",
+            123, 3, "12");
+    test("iota_should_return_full_123_given_buffer_size_is_10",
+            123, 10, "123");
+    test("iota_should_return_-1_out_of_-123_given_buffer_size_is_3",
+            -123, 3, "-1");
+    test("iota_should_return_full_-123_given_buffer_size_is_10",
+            -123, 10, "-123");
+    test("iota_should_return_INT_MAX_given_INT_MAX",
+            INT_MAX, 16, "2147483647");
+    test("iota_should_return_INT_MIN+1_given_INT_MIN+1",
+            INT_MIN+1, 16, "-2147483647");
+    test("iota_should_return_-_given_INT_MIN_due_to_overflow",
+            INT_MIN, 16, "-");
+
+    printf("-TOTAL-: %d/%d\n", n_success, n_runs);
 }
